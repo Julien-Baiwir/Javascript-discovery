35179064aa075508cd8b1349248eda9c777ab8d1
Julien Baiwir <julien.freddy.baiwir@gmail.com>
2023-12-08 15:58:48 +0100

    1

--- exos3.0.js
+++ exos3.0.js
@@ -41,3 +41,29 @@
 // for (let i = 0; i < arr1.length; i++) {
 //     arr2.push(arr1[i]);
 // }
+
+// 3.4 --------------
+
+// let array = [3, 7, 1, 9, 4, 5, 10, 8];
+
+// if (array.length === 0) {
+//   console.log("Array is empty");
+// } else {
+//   let min = array[0];
+//   let max = array[0];
+
+//   for (let i = 1; i < array.length; i++) {
+//     if (array[i] < min) {
+//       min = array[i];
+//     }
+//     if (array[i] > max) {
+//       max = array[i];
+//     }
+//   }
+
+//   console.log(`The minimum element is: ${min}`);
+//   console.log(`The maximum element is: ${max}`);
+// }
+
+// explication:
+// let min = array[0]; let max = array[0];: Ici, deux variables min et max sont initialisées avec la première valeur du tableau. Cela prépare les variables pour comparer les autres éléments du tableau.
