802e5ddd5e20e91fd9626261816e093dc1b6222c
Julien Baiwir <julien.freddy.baiwir@gmail.com>
2023-12-08 13:20:27 +0100

    okay

---
+++ exos3.0.js
@@ -0,0 +1,43 @@
+// 3.0 --------------
+// let arr = ["Joshua", "Pierre", "Thomas", "Huseyine", "Mathias", "Giuseppe"];
+// let firstIteration = true;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (firstIteration) {
+//         console.log("La classe wilson3:");
+//         firstIteration = false; 
+//     }
+//     console.log(arr[i]);
+// }
+
+// 3.1 --------------
+
+// let arr = [1,2,3];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// console.log(sum);
+
+// 3.2 --------------
+// let arr = [1,2,3,4];
+// let sum = 0;
+//  for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// let averagevalue = (sum /arr.length );
+// console.log (averagevalue);
+
+// 3.3 --------------
+// let arr1 = [1,2,3,4,5];
+// let arr2 = [];
+// for (let i = 0; i < arr1.length; i++) {
+//     arr2[i] = arr1[i];
+// }
+// version 2----------
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [];
+
+// for (let i = 0; i < arr1.length; i++) {
+//     arr2.push(arr1[i]);
+// }
