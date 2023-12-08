5e648e9e248298e3613e5e81bae5db463566da50
Julien Baiwir <julien.freddy.baiwir@gmail.com>
2023-12-08 09:36:38 +0100

    okay

---
+++ exos1.01.js
@@ -0,0 +1,27 @@
+// const htmlBadges = 10;
+
+// console.log(htmlBadges);
+
+// let age = 23;
+
+// let sisterAge = age + 11;
+
+// age += 12; 
+
+// console.log(age);
+
+// let htmlBadges = new Number('5');
+
+// let cssBadges = new Number('7');
+
+// let totalBadges = htmlBadges + cssBadges;
+
+// console.log('Woaw, you have ' + totalBadges + "!");
+
+// console.log('10' + '5'); 
+
+// const name = "Lambert";
+// const firstname = "Gerard";
+// const city ="City";
+
+// console.log(`Your name is ${name} ${firstname} and you live in ${city}.`);
---
+++ exos2.0.js
@@ -0,0 +1,95 @@
+
+
+// 2.1--------------------------
+
+//  const userAge = (prompt("Please enter your age:"));
+
+// if (userAge >= 18) {
+//     alert("You are an adult");
+// } else {
+//     alert("You are not yet an adult");
+// }
+
+// let sign = prompt("What's your sign?");
+
+// if (sign.toLowerCase() === "scorpio") {
+//   alert("Wow! I'm a Scorpio too!");
+// }
+
+
+// 2.2 ------------------------------
+
+// let minnumber = prompt("What's your min number?");
+// let maxnumber = prompt("What's your max number?");
+// let currentnumber = prompt("What's your current number?");
+
+// if (minnumber > maxnumber) {
+//     console.log("Error: The minimum number cannot be greater than the maximum number.");
+// } else {
+//     if (currentnumber >= minnumber && currentnumber <= maxnumber) {
+//         console.log(`The current number ${currentnumber} is between ${minnumber} and ${maxnumber}.`);
+//     } else {
+//         console.log(`The current number ${currentnumber} is not between ${minnumber} and ${maxnumber}.`);
+//     }
+// }
+
+
+
+// while
+// let i = 1;
+
+//  while (i <= 100) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// let i = 1
+
+
+
+// 2.4 -----------
+
+// for (let i = 1; i <= 100; i++) 
+//     if (i % 2 === 0) {
+//         console.log(`${i} divided by 2 is ${i / 2}`);
+//     } else {
+//         console.log(`${i} multiplied by 3 is ${i * 3}`);
+//     }
+// ----------------
+
+// for (let i = 1; i <= 100; i += 1) {
+//     if (i % 2 === 0) {
+//                 console.log(`${i} divided by 2 is ${i / 2}`);
+//             } else {
+//                console.log(`${i} multiplied by 3 is ${i * 3}`);
+//            }
+// }
+
+
+
+// for(i=1; i<=100; i++){
+//     if (i%2==0){
+//     console.log(i/2);
+//     }
+//     else{
+//     console.log(i*3);
+//     }
+//     }
+
+// 2.5 ---------------
+
+  
+// let number = prompt('Quel est votre numéro préféré ?');
+// while ( number != 42){
+// alert('Etes-vous sur ?');
+// number = prompt('Quel est votre numéro préféré ?');
+// }
+// if (number == 42){
+//     console.log('yes');
+// }
+
+
+// 2.6 ---------------
+
+
+
